3a854e32a49da8bc76c95ad3242019a84e900ba0
lzangari <lzcazangari.93@gmail.com>
2023-06-30 22:39:56 +0200

    add anotation for significance level

--- correlation_analysis.R
+++ correlation_analysis.R
@@ -14,12 +14,26 @@ decompose_and_plot_acf <- function(df, column, lag_max, path, frequency = 4) {
     ggsave(paste0(column, "_decomposed_plot.svg"), plot = plot(decomposed), dpi = 300,
                                                     width = 9, height = 5, path = path)
 
-    # Plot the ACF of the residuals
-    acf_plot_residual <- ggAcf(decomposed$random, lag.max = lag_max)
+    # calculating the 1 and 10 percent significance levels
+    threshold_1 <- 2.58/sqrt(length(df[[column]]))  # 1% significance level
+    threshold_5 <- 2/sqrt(length(df[[column]]))     # 5% significance level
+    threshold_10 <- 1.645/sqrt(length(df[[column]]))  # 10% significance level
+
+    legend_df <- data.frame(
+    y = c(NA, NA, NA),
+    color = c("1% significance level", "5% significance level", "10% significance level")
+    )
+    # Plot the ACF of the residuals and add the significance levels and show it as a legend
+    acf_plot_residual <- ggAcf(decomposed$random, lag.max = lag_max) +
+                    geom_hline(yintercept = c(-threshold_1, threshold_1), linetype = "dashed", color = "red") +
+                    geom_hline(yintercept = c(-threshold_5, threshold_5), linetype = "dashed", color = "blue") +
+                    geom_hline(yintercept = c(-threshold_10, threshold_10), linetype = "dashed", color = "green") +
+                    annotate("text", x = 45, y = -0.125, label = "1% significance", color = "red") +
+                    annotate("text", x = 45, y = -0.15, label = "5% significance", color = "blue") +
+                    annotate("text", x = 45, y = -0.175, label = "10% significance", color = "green")
     # Save the ACF plot
     ggsave(paste0(column, "_acf_plot_residual.svg"), plot = acf_plot_residual, dpi = 300,
                                                     width = 7, height = 5, path = path)
-
     # Plot the ACF of the trend
     acf_plot_trend <- ggAcf(decomposed$trend, lag.max = lag_max)
     # Save the ACF plot
