5c9e86234a64c162983f6fb6bf65c7e25add568c
lzangari <lucazangari.93@gmail.com>
2023-07-03 22:41:39 +0200

    add stationary function

--- monthly_data_study.R
+++ monthly_data_study.R
@@ -4,8 +4,6 @@
 library(tseries)
 library(ggplot2)
 library(dplyr)
-library(urca)
-library(scales)
 library(forecast)
 library(gridExtra)
 library(gridSVG)
@@ -25,9 +23,30 @@ Sys.setlocale("LC_TIME", "C")
 save_plots <- "C:/Repositories/lasso_oil_price_forcast/plots/monthly"
 df <- read.csv("C:/Repositories/lasso_oil_price_forcast/Data/csv/all_data_cleaned.csv")
 
-#######################################################################
-###--------------------- Check Autocorrelation ---------------------###
-#######################################################################
+
+
+analysis_stationary <- function(df, save_plots, y_column, plot_name, indicator,
+                                                            lag_max=24, frequency=12){
+    # visualize them
+    create_monthly_plot(df = df, path = save_plots, y_column = y_column,
+                                            plot_name = plot_name)
+    # decompose the time series and plot the ACF of the residuals, trend and seasonal component
+    decompose_and_plot_acf(df = df, column = y_column,
+                            lag_max = 24, path = save_plots, frequency = 12)
+    # perform ADF test
+    adf_result <- perform_adf_analysis(df = df, column = y_column,
+                                                indicator = indicator)
+
+    return(adf_result)
+
+}
+
+# add crude oil return price
+df[["one_lag_oil_return_price"]] <- (df[["oil_price"]]/lag(df[["oil_price"]]) -1) *100
+
+# #######################################################################
+# ###------------- Check Autocorrelation with original ---------------###
+# #######################################################################
 plot_names <- list("oil_price" = "WTI Oil price [USD per barrel]",
                     "oil_stock" = "Ending Stocks of Crude Oil [Thousand Barrels]",
                     "oil_export" = "U.S. Exports of Crude Oil [Thousand Barrels]",
@@ -45,6 +64,7 @@ plot_names <- list("oil_price" = "WTI Oil price [USD per barrel]",
                     "gdp_eu" = "EU GDP",
                     "gdp_usa" = "U.S. GDP",
                     "federal_fund" = "Federal Funds Rate [%]",
+                    "tb3ms" = "3-Month Treasury Bill: Secondary Market Rate [%]",
                     "sp500" = "S&P 500 Index",
                     "msci" = "MSCI World Index",
                     "stoxx600" = "EURO STOXX 600 Index",
@@ -52,7 +72,8 @@ plot_names <- list("oil_price" = "WTI Oil price [USD per barrel]",
                     "gold_price" = "Gold Price [USD per ounce]",
                     "copper_future" = "Copper Future [USD per pound]",
                     "geopolitical_risk" = "Geopolitical Risk Index",
-                    "wui" = "World Uncertainty Index")
+                    "wui" = "World Uncertainty Index",
+                    "one_lag_oil_return_price" = "WTI oil return price [%]")
 
 short_names <- list("oil_price" = "Oil Price",
                     "oil_stock" = "U.S. Ending Stocks",
@@ -71,6 +92,7 @@ short_names <- list("oil_price" = "Oil Price",
                     "gdp_eu" = "EU GDP",
                     "gdp_usa" = "U.S. GDP",
                     "federal_fund" = "Federal Funds Rate",
+                    "tb3ms" = "3-Month Treasury Bill",
                     "sp500" = "S&P 500",
                     "msci" = "MSCI World",
                     "stoxx600" = "EURO STOXX 600",
@@ -78,29 +100,109 @@ short_names <- list("oil_price" = "Oil Price",
                     "gold_price" = "Gold",
                     "copper_future" = "Copper Future",
                     "geopolitical_risk" = "GRI",
-                    "wui" = "WUI")
-# do analysis for each data
+                    "wui" = "WUI",
+                    "one_lag_oil_return_price" = "Retrun price [1 lag]")
+# # do analysis for each data
 adf_results <- list()
+hetero_results <- list()
 for (name_index in 2:length(names(df))){
-    # visualize them
-    create_monthly_plot(df = df, path = save_plots, y_column = names(df)[name_index], plot_name = plot_names[[names(df)[name_index]]])
-    # decompose the time series and plot the ACF of the residuals, trend and seasonal component
-    decompose_and_plot_acf(df = df, column = names(df)[name_index],
-                            lag_max = 24, path = save_plots, frequency = 12)
-    # perform ADF test
-    adf_result <- perform_adf_analysis(df = df, column = names(df)[name_index], indicator = short_names[[names(df)[name_index]]])
+    adf_result <- analysis_stationary(df = df, save_plots = save_plots,
+                y_column = names(df)[name_index], plot_name = plot_names[[names(df)[name_index]]],
+                    indicator = short_names[[names(df)[name_index]]])
+
     adf_results[[name_index]] <- adf_result
+
+    hetero_result <- analysis_heteroscedacity(indicator = names(df)[name_index],
+            predictor_index = 28, df = df, name_index = name_index)
+    hetero_results[[name_index]] <- hetero_result
+
 }
 
+# assemble the table for adf_table
+create_svg_from_table(results = adf_table, name = "monthly_column_adf_table.svg")
 
 # assemble the table
-adf_table <- do.call(rbind, adf_results)
-table_grob <- tableGrob(adf_table, theme = ttheme_default(base_size = 8, base_line_size = 0.5))
-grDevices::svg("monthly_column_adf_table.svg", width = 10, height = 7)
+hetero_table <- do.call(rbind, hetero_results)
+table_grob <- tableGrob(hetero_table, theme = ttheme_default(base_size = 8, base_line_size = 0.5))
+grDevices::svg("monthly_column_bptest_table.svg", width = 9, height = 7)
 grid.newpage()
 grid.draw(table_grob)
-grid.export("monthly_column_adf_table.svg")
+grid.export("monthly_column_bptest_table.svg")
 dev.off()
+# #######################################################################
+# ###------------- Check Autocorrelation with log ---------------###
+# #######################################################################
+# adf_results_log <- list()
+# for (i in 2:ncol(df)) {
+#     # Get the column name
+#     column_name <- names(df)[i]
+#     # Create the new column name
+#     new_column_log_name <- paste0('log_', column_name)
+#     # Compute the log of the column and add it to the data frame
+#      df[[new_column_log_name]] <- log(df[[column_name]])
+
+#     # # visualize them
+#     # create_monthly_plot(df = df, path = save_plots, y_column = new_column_log_name,
+#     #                                         plot_name = plot_names[[column_name]])
+#     # # decompose the time series and plot the ACF of the residuals, trend and seasonal component
+#     # decompose_and_plot_acf(df = df, column = new_column_log_name,
+#     #                         lag_max = 24, path = save_plots, frequency = 12)
+#     # # perform ADF test
+#     # adf_result <- perform_adf_analysis(df = df, column = new_column_log_name,
+#     #                                             indicator = short_names[[column_name]])
+#     ########################## problem in ts_data
+#     adf_result_log <- analysis_stationary(df = df, save_plots = save_plots,
+#                 y_column = new_column_log_name,
+#                 plot_name = paste0("log"," ", plot_names[[column_name]]),
+#                     indicator = short_names[[column_name]])
+#     ########################## problem
+
+#     adf_results_log[[i]] <- adf_result_log
+# }
+
+# # assemble the table
+# adf_table_log <- do.call(rbind, adf_results_log)
+# table_grob <- tableGrob(adf_table_log, theme = ttheme_default(base_size = 8, base_line_size = 0.5))
+# grDevices::svg("monthly_column_adf_table_log.svg", width = 9, height = 7)
+# grid.newpage()
+# grid.draw(table_grob)
+# grid.export("monthly_column_adf_table_log.svg")
+# dev.off()
+
+
+
+#######################################################################
+###------------- Check Autocorrelation with lag ---------------###
+#######################################################################
+adf_results_lag_one <- list()
+for (i in 2:ncol(df)) {
+    # Get the column name
+    column_name <- names(df)[i]
+    # Create the new column name
+    new_column_lag_one_name <- paste0('lag_one_', column_name)
+    # Compute the log of the column and add it to the data frame
+     df[[new_column_lag_one_name]] <- log(df[[column_name]])
+
+    ########################## problem in ts_data
+    adf_result_lag_one <- analysis_stationary(df = df, save_plots = save_plots,
+                y_column = new_column_lag_one_name,
+                plot_name = paste0("lag one"," ", plot_names[[column_name]]),
+                    indicator = short_names[[column_name]])
+    ########################## problem
+
+    adf_results_lag_one[[i]] <- adf_result_lag_one
+}
+
+# assemble the table
+adf_table_lag_one <- do.call(rbind, adf_results_lag_one)
+table_grob <- tableGrob(adf_table_lag_one, theme = ttheme_default(base_size = 8, base_line_size = 0.5))
+grDevices::svg("monthly_column_adf_table_lag_one.svg", width = 9, height = 7)
+grid.newpage()
+grid.draw(table_grob)
+grid.export("monthly_column_adf_table_lag_one.svg")
+dev.off()
+
+
 
 #######################################################################
 ###------------------------ Transformation  --.---------------------###
@@ -132,3 +234,44 @@ dev.off()
 # adf.test(na.omit(log_diff_12_data))
 # monthly_us_net_imports_of_crude_oil_and_petroleum_products <- na.omit(monthly_us_net_imports_of_crude_oil_and_petroleum_products)
 # adf.test(monthly_us_net_imports_of_crude_oil_and_petroleum_products)
+
+
+
+# # Assuming df is your data frame
+# for (i in 2:ncol(df)) {
+#   # Get the column name
+#   column_name <- names(df)[i]
+#   # Create the new column name
+#   new_column_name <- paste0('log_', column_name)
+#   # Compute the log of the column and add it to the data frame
+#   df[[new_column_name]] <- log(df[[column_name]])
+# }
+
+
+# # Assuming df is your data frame
+# constant = 1  # Define your constant here
+
+# for (i in 2:ncol(df)) {
+#   # Get the column name
+#   column_name <- names(df)[i]
+#   # Create the new column name
+#   new_column_name <- paste0('log_', column_name)
+#   # Compute the log of the column (plus the constant) and add it to the data frame
+#   df[[new_column_name]] <- log(df[[column_name]] + constant)
+# }
+
+
+# library(dplyr)
+
+# # Assume 'df' is your data frame and 'price' is the column with the oil prices
+# df <- df %>%
+#   mutate(return = (price/lag(price) - 1) * 100)
+
+
+# library(dplyr)
+
+# # Assume 'df' is your data frame and 'price' is the column with the oil prices
+# df <- df %>%
+#   mutate(return_2 = (price/lag(price, 2) - 1) * 100,
+#          return_4 = (price/lag(price, 4) - 1) * 100,
+#          return_8 = (price/lag(price, 8) - 1) * 100)
